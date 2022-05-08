15d2f028a2dc565f6c3777daea02a1f51d2f6ff6
Rawle Springer <rspringerdeveloper@gmail.com>
2022-05-08 16:46:24 -0400

    Add HTM structure to NewsCard Component

--- components/NewsCard.js
+++ components/NewsCard.js
@@ -1,7 +1,20 @@
 import styles from '../styles/NewsCard.module.css';
+import GuardianImg from './GuardianLogo';
 
 const NewsCard = () => {
-  return <article>NEWS ARTICLE</article>;
+  return (
+    <article className={styles.container}>
+      <div>
+        <GuardianImg />
+      </div>
+
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eaque commodi
+        explicabo, illum doloribus quasi ea voluptatibus assumenda? Vel non quos
+        sint iure similique ex cumque vero saepe. Vitae, at. Minima!
+      </p>
+    </article>
+  );
 };
 
 export default NewsCard;
