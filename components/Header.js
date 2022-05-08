763a308dd80d2e00e7db58801eb826e7cc28d80f
Rawle Springer <rspringerdeveloper@gmail.com>
2022-05-08 14:20:19 -0400

    Add Logo and fill out content of Header including the necessary html and css

--- components/Header.js
+++ components/Header.js
@@ -1,5 +1,32 @@
+import Link from 'next/link';
+import Logo from './Logo';
+import styles from '../styles/Header.module.css';
+
 const Header = () => {
-  return <header>This is the header</header>;
+  return (
+    <header className={styles.nav}>
+      <Logo />
+      <nav>
+        <ul>
+          <li>
+            <Link href="#">
+              <a>National</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="#">
+              <a>Business</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="#">
+              <a>Sports</a>
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 };
 
 export default Header;
---
+++ components/Logo.js
@@ -0,0 +1,7 @@
+import logo from '../assets/logo.svg';
+
+const Logo = () => {
+  return <img src={logo.src} height="50px" width="250px" />;
+};
+
+export default Logo;
